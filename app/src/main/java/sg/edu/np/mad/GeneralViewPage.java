445f82b74d1aeb0b1b19d89e04012dcb5bd17447
leroy <s10243705@connect.np.edu.sg>
2023-06-12 16:40:26 +0800

    new

--- app/src/main/java/sg/edu/np/mad/GeneralViewPage.java
+++ app/src/main/java/sg/edu/np/mad/GeneralViewPage.java
@@ -14,6 +14,7 @@ public class GeneralViewPage extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList<Store> storeList = new ArrayList<>();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,4 +27,19 @@ public class GeneralViewPage extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
     }
+
+    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+        @Override
+        public boolean onQueryTextSubmit(String query){
+            searchList=new ArrayList<>();
+            if (query.length()>0){
+                for(int i=0;i <arrayList.size();i++){
+                    if (arrayList.get(1). getFruitName(). toUpperCase(). contains(query. toUpperCase() ) ){
+                        ModelClass modelClass=new ModelClass();modelClass.setFruitName(arrayList.get(i).getFruitName());modelClass. setFruitNum(arrayList. get(i).getFruitNum());modelClass.setImg(arrayList.get(i).getImg());searchList. add(modelClass);
+                        {
+                            {
+                                RecyclerView.LayoutManager layoutManager=new LinearLayoutManager( context: MainActivity.this);recyclerView.setLayoutManager(layoutManager) ;
+                                FruitAdapter fruitAdapter=new FruitAdapter( context: MainActivity.this,searchList);recyclerView. setAdapter(fruitAdapter);
+                            }
+                            return false;
 }
