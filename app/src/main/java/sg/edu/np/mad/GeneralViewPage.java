097cf28bfbe39c0d4ea247cf073534aae89c0127
leroy <s10243705@connect.np.edu.sg>
2023-06-12 11:46:47 +0800

    new

--- app/src/main/java/sg/edu/np/mad/GeneralViewPage.java
+++ app/src/main/java/sg/edu/np/mad/GeneralViewPage.java
@@ -1,15 +1,21 @@
 package sg.edu.np.mad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class GeneralViewPage extends AppCompatActivity {
 
+    SearchView searchView;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_view_page);
+        recyclerView=findViewById(R.id.recyclerView);
+        searchView=findViewById(R.id.searchView);
     }
 
     @Override
