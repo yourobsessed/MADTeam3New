41cb1a427380168b6f99772706ffadf915f3a858
leroy <s10243705@connect.np.edu.sg>
2023-06-12 11:35:38 +0800

    new

---
+++ app/src/main/java/sg/edu/np/mad/GeneralViewPage.java
@@ -0,0 +1,19 @@
+package sg.edu.np.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneralViewPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_view_page);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+}
