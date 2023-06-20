11695e8d6010ebd28fffe8be687be2c880505d96
Jovin <s10243666@connect.np.edu.sg>
2023-06-21 01:07:28 +0800

    updated

--- app/src/main/java/sg/edu/np/mad/GeneralView_Filter.java
+++ app/src/main/java/sg/edu/np/mad/GeneralView_Filter.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -12,6 +13,7 @@ import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GeneralView_Filter extends AppCompatActivity {
 
@@ -23,7 +25,7 @@ public class GeneralView_Filter extends AppCompatActivity {
     private Button buttonApply;
 
     private ArrayList<String> selectedChipData;
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_view_filter);
@@ -66,5 +68,77 @@ public class GeneralView_Filter extends AppCompatActivity {
 
             }
         });
+    }*/
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_view_filter);
+
+        chipGroupLocation = findViewById(R.id.chipGroupLocation);
+        buttonApply = findViewById(R.id.buttonApply);
+        selectedChipData = new ArrayList<>();
+
+        chipGroupLocation.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(ChipGroup group, int checkedId) {
+                Chip chip = group.findViewById(checkedId);
+                if (chip != null) {
+                    if (chip.isChecked()) {
+                        selectedChipData.add(chip.getText().toString());
+                    } else {
+                        selectedChipData.remove(chip.getText().toString());
+                    }
+                }
+            }
+        });
+
+        buttonApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Selected Chips", selectedChipData.toString());
+                // Further processing or sending the selected data
+            }
+        });
+    }*/
+
+    //private ChipGroup chipGroupLocation;
+    //private Button buttonApply;
+    private List<Chip> selectedChips;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_view_filter);
+
+        chipGroupLocation = findViewById(R.id.chipGroupLocation);
+        buttonApply = findViewById(R.id.buttonApply);
+        selectedChips = new ArrayList<>();
+
+        for (int i = 0; i < chipGroupLocation.getChildCount(); i++) {
+            Chip chip = (Chip) chipGroupLocation.getChildAt(i);
+            chip.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (selectedChips.contains(chip)) {
+                        selectedChips.remove(chip);
+                    } else {
+                        selectedChips.add(chip);
+                    }
+                    chip.setChecked(!chip.isChecked());
+                }
+            });
+        }
+
+        buttonApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<String> selectedChipData = new ArrayList<>();
+                for (Chip chip : selectedChips) {
+                    selectedChipData.add(chip.getText().toString());
+                }
+                // Perform further processing or send the selected data
+            }
+        });
     }
+
 }
