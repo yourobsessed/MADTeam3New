aef45eccee451fdcffbdc84ede65711eeae94f66
Jovin <s10243666@connect.np.edu.sg>
2023-07-06 16:01:05 +0800

    updated

---
+++ app/src/main/java/sg/edu/np/mad/WishList_ViewHolder.java
@@ -0,0 +1,17 @@
+package sg.edu.np.mad;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+
+public class WishList_ViewHolder {
+    TextView Name;
+    TextView desc;
+    ImageView Image;
+    TextView Location;
+    CardView cardView;
+
+
+
+}
