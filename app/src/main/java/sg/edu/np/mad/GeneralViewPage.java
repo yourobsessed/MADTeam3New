2dde59ea167c05e10b52ce67db5219154f8869e4
leroy <s10243705@connect.np.edu.sg>
2023-07-29 23:12:06 +0800

    updates

--- app/src/main/java/sg/edu/np/mad/GeneralViewPage.java
+++ app/src/main/java/sg/edu/np/mad/GeneralViewPage.java
@@ -468,6 +468,7 @@ public class GeneralViewPage extends AppCompatActivity implements SelectListener
 
             }
         }
+
         /*if (filteredList.isEmpty()) {
             Toast.makeText(this,"No data found!",Toast.LENGTH_SHORT).show();
         }*/
